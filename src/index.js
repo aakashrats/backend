28beec598551c18bf56de3e9864822dd88c0ec78
aakashrats <rathodaakash001@gmail.com>
2024-09-12 00:19:27 +0530

    connect to mongo

---
+++ src/index.js
@@ -0,0 +1,72 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env',
+
+})
+
+connectDB(); 
+
+
+
+
+
+
+
+
+// import express from 'express';
+// const app = express();
+// const PORT = process.env.PORT ;
+
+// app.listen(PORT, () => {
+//   console.log(`App is listening on port ${PORT}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// import mongoose, { Mongoose } from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from "express"
+// const app = express()
+// (async () =>
+// {
+//     try{
+//         await Mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("errror",(error)=>{
+//           console.log("ERRR:", error);
+//            throw error
+//     })
+//     app.listen(process.env.PORT,() +> {
+//         console.log(`App is listening on port ${proces.env.PORT ||8000}`)
+//     })
+
+//     } catch (error) {
+//         console.error("ERROR:",error);
+//         throw err
+//     }
+// })()
+*/
